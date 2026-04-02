efe15fdea4628f4e3c8c0f384ea49c8930d1f353
gaihangyu <gaihangyu_v@didiglobal.com>
2026-04-02 12:53:11 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
-import 'database/db.dart';
+import 'database/db_mobile.dart' if (dart.library.html) 'database/db_web.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
